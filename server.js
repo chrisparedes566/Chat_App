55f4f78e6924ae36d794d4239431e7f958cede8b
Christopher Paredes <Christopher@Christophers-MacBook-Pro.local>
2019-02-09 18:45:53 -0500

    Updated app to match traversy.

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const mongo = require('mongodb').MongoClient;
-const io = require('socket.io').listen(3000).sockets;
+const client = require('socket.io').listen(3000).sockets;
 
 // Connect to mongo
-mongo.connect('mongodb://127.0.0.1', { useNewUrlParser: true },function(err, db){
+mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
     if(err){
         throw err;
     }
-    console.log('MongoDB Connected to Port: ' + 3000);
+
+    console.log('MongoDB connected...');
 
     // Connect to Socket.io
-    io.on('connection', function(socket){
+    client.on('connection', function(socket){
         let chat = db.collection('chats');
 
         // Create function to send status
@@ -17,7 +18,7 @@ mongo.connect('mongodb://127.0.0.1', { useNewUrlParser: true },function(err, db)
             socket.emit('status', s);
         }
 
-        //Get chats from mongo collection
+        // Get chats from mongo collection
         chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
             if(err){
                 throw err;
@@ -27,25 +28,26 @@ mongo.connect('mongodb://127.0.0.1', { useNewUrlParser: true },function(err, db)
             socket.emit('output', res);
         });
 
-        // Handle input event
+        // Handle input events
         socket.on('input', function(data){
             let name = data.name;
             let message = data.message;
 
             // Check for name and message
-            if(name =='' || message == '') {
-                sendStatus('Please provide a name and message');
+            if(name == '' || message == ''){
+                // Send error status
+                sendStatus('Please enter a name and message');
             } else {
                 // Insert message
                 chat.insert({name: name, message: message}, function(){
-                    io.emit('output', [data]);
+                    client.emit('output', [data]);
 
                     // Send status object
                     sendStatus({
                         message: 'Message sent',
                         clear: true
                     });
-                })
+                });
             }
         });
 
@@ -53,8 +55,9 @@ mongo.connect('mongodb://127.0.0.1', { useNewUrlParser: true },function(err, db)
         socket.on('clear', function(data){
             // Remove all chats from collection
             chat.remove({}, function(){
-                socket.emit('Cleared');
-            })
-        })
+                // Emit cleared
+                socket.emit('cleared');
+            });
+        });
     });
 });
